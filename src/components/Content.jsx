4f7db61d13cb2f672ff4ee7dcf466009dfce32ca
Delon Henrique <delon.henrique@gmail.com>
2022-08-22 21:33:44 -0300

    API request

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import Craft from "./Craft";
 import Home from "./Home";
+import "./Content.css"
 
 const Content = (props) => {
-    
+
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/craft" element={<Craft />} />
-        </Routes>
+        <div className="content">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/craft" element={<Craft />} />
+            </Routes>
+        </div>
     )
 }
 
--- src/components/Craft.jsx
+++ src/components/Craft.jsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import "./Craft.css"
 
 const Craft = (props) => {
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([])
 
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const { data: response } = await axios.get('https://www.albion-online-data.com/api/v2/stats/Prices/T8_BAG@3.json?qualities=1%2C2');
+                setData(response);
+            } catch (error) {
+                console.error(error.message);
+            }
+            setLoading(false);
+        }
+        fetchData();
+    }, []);
     return (
         <div>
-            <h1>Craft</h1>
+            {loading && <div className='loading'>Loading...</div>}
+            {!loading && (
+                <div>
+                    <h2>Dados da API...</h2>
+                    <table>
+                        <tr>
+                            <th>Item</th>
+                            <th>City</th>
+                            <th>sell price min</th>
+                        </tr>
+                        {data.map(item => (
+                            <tr>
+                                <td>
+                                    <span>{item.item_id}</span>
+                                </td>
+                                <td>
+                                    <span>{item.city}</span>
+                                </td>
+                                <td>
+                                    <span>{item.sell_price_min}</span>
+                                </td>
+                            </tr>
+                        ))}
+                    </table>
+                </div>
+            )}
         </div>
     )
 }
